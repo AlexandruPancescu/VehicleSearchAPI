b4752df4e9a9ff27a1b5333c3210c70708ef8316
AlexandruPancescu <98697528+AlexandruPancescu@users.noreply.github.com>
2023-07-25 19:52:08 +0300

    Initial project setup

---
+++ src/main/java/com/learnSpring/vehicle/api/vehicleSearchAPI/VehicleSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.learnSpring.vehicle.api.vehicleSearchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnSpring/vehicle/api/vehicleSearchAPI/VehicleSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnSpring.vehicle.api.vehicleSearchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
